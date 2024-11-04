625cec9ce6e103e40bfdaed335b00043b27c8ffe
Augusto Oliveira <AugustoO@pronamics.com.au>
2024-11-04 12:54:51 +1000

    Initialize the backend
    Beggining the API with .NET

---
+++ TravelMapApi/Models/Location.cs
@@ -0,0 +1,8 @@
+public class Location
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string Description { get; set; }
+    public List<Pin> Pins { get; set; } = new();
+}
---
+++ TravelMapApi/Models/Pin.cs
@@ -0,0 +1,9 @@
+public class Pin
+{
+    public int Id { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public int LocationId { get; set; }
+    public Location Location { get; set; }
+    public List<Trip> Trips { get; set; } = new();
+}
